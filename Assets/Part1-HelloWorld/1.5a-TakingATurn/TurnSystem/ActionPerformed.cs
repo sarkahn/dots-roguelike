028651725b73f9f303ae96be8aa4a22315ebf235
sark <sarkahn@gmail.com>
2020-02-22 10:13:28 -0700

    Updated to 2019.3.2f1

--- Assets/Part1-HelloWorld/1.5a-TakingATurn/TurnSystem/ActionPerformed.cs
+++ Assets/Part1-HelloWorld/1.5a-TakingATurn/TurnSystem/ActionPerformed.cs
@@ -10,6 +10,8 @@ namespace RLTKTutorial.Part1_5A
     [System.Serializable]
     public struct ActionPerformed : IComponentData
     {
+        public bool value;
+
         public int cost;
     }
 }
--- Assets/Part1-HelloWorld/1.5a-TakingATurn/TurnSystem/TakingATurn.cs
+++ Assets/Part1-HelloWorld/1.5a-TakingATurn/TurnSystem/TakingATurn.cs
@@ -6,7 +6,17 @@ using UnityEngine;
 
 namespace RLTKTutorial
 {
+    //public struct TakingATurn : IComponentData
+    //{
+    //}
+
+
+    [System.Serializable]
     public struct TakingATurn : IComponentData
     {
+        public bool value;
+        public static implicit operator bool(TakingATurn c) => c.value;
+        public static implicit operator TakingATurn(bool v) => new TakingATurn { value = v };
     }
+
 }
